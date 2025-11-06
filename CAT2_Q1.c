710ced0238b917051d3d67ee586ce9eb82ae5bd8
markian001 <markmurithi284@gmail.com>
2025-11-06 10:34:40 +0300

    Update CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -5,22 +5,19 @@ Description:A program that that prints elements of array using a nested for loop
 */
 
 #include <stdio.h>
-
 int main()
 {
     int scores[2][2] = {
        {65, 92},
        {84, 72}
-};
-
-printf("Elements of the scores array:\n");
-
-for(int i = 0; i < 2; i++) {
-    for(int j = 0; j < 2; j++){
-        printf("%d ", scores[i][j]);
+    };
+    
+    printf("Elements of the scores array:\n");
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++){
+            printf("%d ", scores[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-}
-
-return 0;
+    return 0;
 }
