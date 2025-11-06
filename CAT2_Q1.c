e8fc02ee86846ed947c5738dd700051d98caf439
markian001 <markmurithi284@gmail.com>
2025-11-06 08:50:30 +0300

    Update and rename CAT2_Q1.c.cbp to CAT2_Q1.c

---
+++ CAT2_Q1.c
@@ -0,0 +1,26 @@
+/*
+Name:Mark Ian Murithi
+Reg No:CT101/G/26541/25
+Description:A program that that prints elements of array using a nested for loop
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int scores[2][2] = {
+       {65, 92},
+       {84, 72}
+};
+
+printf("Elements of the scores array:\n");
+
+for(int i = 0; i < 2; i++) {
+    for(int j = 0; j < 2; j++){
+        printf("%d ", scores[i][j]);
+    }
+    printf("\n");
+}
+
+return 0;
+}
